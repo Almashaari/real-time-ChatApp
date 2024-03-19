77553823b5dc108db3bd9938108438aff50d73bb
AnwarNa <anwar.se19@gmail.com>
2024-03-19 05:54:59 +0300

    added chat functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { React, useRef, useState } from 'react';
+import { React, useState } from 'react';
 import Cookies from "universal-cookie";
+import "./App.css";
 import Auth from './component/Auth';
 import Chat from './component/Chat';
 const cookies = new Cookies();
 function App() {
   const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
-  const [room, setRoom] = useState("");
-  const roomInputRef = useRef(null);
+
   if (!isAuth) {
   return (
     <div className="App">
@@ -16,19 +16,9 @@ function App() {
   );
   }
   return (
-
-   <div>{room ? ( <Chat room={room} />) :(
-
-    <div className="room">
-    <label> Type room name: </label>
-    <input ref={roomInputRef} />
-    <button onClick={() => setRoom(roomInputRef.current.value)}>
-      Enter Chat
-    </button>
-  </div>
-   )}
-
-      </div>
+    <div className="App">
+      <Chat />
+    </div>
   );
 
 }
--- src/component/Chat.jsx
+++ src/component/Chat.jsx
@@ -1,15 +1,34 @@
 import {
   addDoc,
   collection,
+  onSnapshot,
+  orderBy,
+  query,
   serverTimestamp,
 } from "firebase/firestore";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { auth, db } from "../firebase-config";
-function Chat(props) {
-  const {room} = props;
+import "../styles/Chat.css";
+
+function Chat() {
+  const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
   const messagesRef = collection(db, "messages");
 
+  useEffect(() => {
+    const queryMessages = query(messagesRef, orderBy("createdAt")); 
+
+    const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(messages); // Optional for debugging
+      setMessages(messages);
+    });
+
+    return unsubscribe;
+  }, []); 
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -19,8 +38,6 @@ function Chat(props) {
       text: newMessage,
       createdAt: serverTimestamp(),
       user: auth.currentUser?.displayName,
-
-      room,
     });
 
     setNewMessage("");
@@ -28,6 +45,20 @@ function Chat(props) {
 
   return (
     <div className="chat-app">
+      <div className="header">
+        <h1>SPEECH</h1>
+      </div>
+      <div className="messages">
+        {messages.length > 0 ? (
+          messages.map((message) => (
+            <div key={message.id} className="message">
+              <span className="user">{message.user}:</span> {message.text}
+            </div>
+          ))
+        ) : (
+          <p>No messages yet...</p>
+        )}
+      </div>
       <form onSubmit={handleSubmit} className="new-message-form">
         <input
           value={newMessage}
